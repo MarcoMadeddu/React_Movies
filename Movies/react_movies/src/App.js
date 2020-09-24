12068951fe64f2d6e2e0adf6a4402ddd0fa15372
MarcoMadeddu <marcomade99@gmail.com>
2020-09-24 18:28:19 +0200

    Add Fetch API

--- Movies/react_movies/src/App.js
+++ Movies/react_movies/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component}from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//CONST & VARS
+const APIKEY = '8ed69995';
+const APIURL = `http://www.omdbapi.com`;
+
+class App extends Component {
+  componentDidMount(){
+    fetch(APIURL + '?apikey=' + APIKEY + '&s=back to the future').then( res => res.json())
+    .then(apiResult => console.log(apiResult));
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hellooo</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
